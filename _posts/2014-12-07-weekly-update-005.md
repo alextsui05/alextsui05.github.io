1dca782cb9b0e8ce9e5397d3c321729327e7765e
Alex Tsui <alextsui05@gmail.com>
2014-12-07 23:47:07 -0800

    Add weekly update, and also a weekly update posting script.

---
+++ _posts/2014-12-07-weekly-update-005.md
@@ -0,0 +1,4 @@
+---
+title: Weekly Update 5
+---
+Still busy with school. 11 days to go on this paper deadline.
---
+++ weekly-update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+X=$(ls _posts/*weekly-update* | tail -n1 | cut -d- -f6)
+X=$(echo ${X%%.md} | sed s/^0*//)
+X2=$(printf "%03d" $((X + 1)))
+DATE=$(date +%Y-%m-%d)
+cat > _posts/${DATE}-weekly-update-${X2}.md << EOF
+---
+title: Weekly Update $(echo $((X + 1)))
+---
+
+EOF
+vi _posts/${DATE}-weekly-update-${X2}.md
