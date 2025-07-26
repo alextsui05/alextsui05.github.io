63ba10ed1f34e28a5ef0fd6b511b4aa4ce14a198
Alex Tsui <alextsui@pm.me>
2025-07-26 16:58:56 -0700

    Introduce environment-specific configuration
    - Specify `backstage.url` as a parameter
    - Embed the value of `backstage.url` in an HTML meta tag
      - Look up meta tag in custom JS and process accordingly

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,14 @@
 (async () => {
+  const backstageUrl = document.querySelector('meta[name="backstage-url"]')?.getAttribute('content')
+  if (!backstageUrl) {
+    return;
+  }
+
   const viewCount = document.querySelector("[data-view-count]");
   if (viewCount) {
     let count = '';
     try {
-      const response = await fetch("https://backstage.atsui.click/counters/1");
+      const response = await fetch(`${backstageUrl}/counters/1`);
       const counter = await response.json();
 
       count = counter.count ?? 0;
